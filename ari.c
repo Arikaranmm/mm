b3669eb527e44cf686ec81be22b529c2eceb87bd
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-01-29 14:18:10 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-
 int main()
-{
+{ 
     char ch;
-    
-    /* Input a character from user */
     printf("Enter any character: ");
     scanf("%c", &ch);
-    
-
-    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+     if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
     {
-        printf("Character is an ALPHABET.");
+        printf(" ALPHABET.");
     }
     else
     {
-        printf("Character is NOT ALPHABET.");
+        printf(" ALPHABET.");
     }
 
     return 0;
